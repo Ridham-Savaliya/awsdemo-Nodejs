a696f1891afd6fa0218abcbf861c457d44d2e10b
Ridham-Savaliya <savaliyaridham46@gmail.com>
2025-01-09 18:51:05 +0530

    API get_user_details added!

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.get("/api/get",(req,res)=>{
 	res.send({message:"welcome back to the nodejs app!."})
 })
 
+app.get("/api/get_user_details",(req,res)=>{
+	res.send({user:{
+		name:"ridham savaliya",
+		age:21,
+		gender:"male",
+		hobby:"sex",
+		degree:"BCA"
+	}})
+})
 app.listen(8000,()=>{
 	console.log("server is running on the port 8000!.")
 })	
